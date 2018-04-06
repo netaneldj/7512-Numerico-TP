01572ae7243dc2f4cbb7e75dc6cb4508a1b90ed6
netaneldj <netaneldj97@gmail.com>
2018-04-06 08:35:34 -0300

    Comienzo metodo Punto-Fijo

---
+++ puntoFijo.py
@@ -0,0 +1,21 @@
+import math
+
+ERROR = math.pow(5,-17)
+#PADRON = 99093
+#L0 = 2*100000/PADRON
+L0 = 2.02 #Redondeado a 3 digitos significativos
+G = 9.81
+K = 10
+M = 0
+A = 1
+
+def punto_fijo(g,inicio,fin,error):
+
+def punto_fijo_rec(g,comienzo,error,raiz):
+	
+def g(y):
+	return y+2*K*y*(1+L0/math.sqrt(y*y+A*A))+M*G 
+
+def main():
+	
+main()
