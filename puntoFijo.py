af8ce167b3c6d52ff7476031306e04154d7a5366
netaneldj <netaneldj97@gmail.com>
2018-04-06 15:16:15 -0300

    Actualizacion metodo punto fijo funcionando

--- puntoFijo.py
+++ puntoFijo.py
@@ -10,12 +10,41 @@ M = 0
 A = 1
 
 def punto_fijo(g,inicio,fin,error):
-
+	raiz = []
+	verificar_acotada(g,inicio,fin)
+	verificar_acotada(derivadaG,inicio,fin)
+	comienzo = calcular_comienzo(inicio,fin)
+	if(punto_fijo_rec(g,comienzo,error,raiz)):
+		return raiz
+	raise ValueError("No hay raiz")
+		
 def punto_fijo_rec(g,comienzo,error,raiz):
+	raiz.append(comienzo)
+	if(abs(f(comienzo))<=ERROR):
+		return True
+	medio = g(comienzo)
+	punto_fijo_rec(g,medio,error,raiz)
+	return True
+
+def verificar_pertenencia_intervalo(g,inicio,fin):
+	return (inicio<=g(inicio)<=fin and inicio<=g(fin)<=fin)
+
+def verificar_acotada(derivadaG,inicio,fin):
+	return (derivadaG(inicio)<=1 and derivadaG(fin)<=1) 
+
+def calcular_comienzo(inicio,fin):
+	return (inicio+fin)/2
+
+def f(y):
+	return -2*K*y*(1-L0/math.sqrt(y*y+A*A))-M*G 
 	
 def g(y):
-	return y+2*K*y*(1+L0/math.sqrt(y*y+A*A))+M*G 
+	return y*L0/math.sqrt(y*y+A*A) - M*G/(2*K)
+
+def derivadaG(y):
+	return (L0*math.sqrt(y*y+A*A)-L0*y*y/math.sqrt(y*y+A*A))/(y*y+A*A)
 
 def main():
-	
+	print(punto_fijo(g,1,2,ERROR))
+
 main()
