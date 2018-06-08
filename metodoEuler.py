cd17dcfa086b9c0a37a0607aa738287a1c4dddea
ailinyelen <ailinyelenb@gmail.com>
2018-06-08 16:18:14 -0300

    metodo euler punto 4

--- metodoEuler.py
+++ metodoEuler.py
@@ -1,23 +1,58 @@
 import math
 
-def euler(a,b,N,y0):
-	h=calcularPaso(a,b,N)
-	y = []
-	y.append(y0)
-	for i in range(N):
-		y.append(y[i] + h*f(calcularPunto(a,i,h),y[i]))
-	return y[N]
-		
-def calcularPaso(a,b,N):
-	return float(b-a)/N
+G = 9.807
+M = 87.464
+L0 = 51.549
+K1 = 49.366
+K2 = 1
 
-def calcularPunto(a,i,h):
-	return a+i*h
+def euler(h, y0, v0):
 
-def f(t,y):
-	return y-t*t+1
+	posicion = []
+	posicion.append(y0)
+
+	velocidad = []
+	velocidad.append(v0)
+
+	aceleracion = []
+	aceleracion.append(calcular_aceleracion(y0))
+
+	oscilacion = 0
+	n = 0
+	cuarta_oscilacion = False
+
+	while not cuarta_oscilacion:
+
+		posicion.append(calcular_posicion(posicion, velocidad, h, n))
+		velocidad.append(calcular_velocidad(posicion, velocidad, h, n))
+		aceleracion.append(calcular_aceleracion(posicion[n+1]))
+
+		n+=1
+
+		if hay_un_maximo(posicion, n):
+			oscilacion+=1
+			if oscilacion == 4:
+				cuarta_oscilacion = True
+
+	print(posicion)
+	print(velocidad)
+	print(aceleracion)
+
+def calcular_posicion(y, v, h, n):
+	return y[n] + h * v[n]
+
+def calcular_velocidad(y, v, h, n):
+	return v[n] + h* calcular_aceleracion(y[n])
+
+def calcular_aceleracion(y):
+	if (y < L0): return G
+	return G - (K1/M) * math.pow(y-L0, K2)
+
+def hay_un_maximo(y, n):
+	if (n > 1) and (y[n] > y[n-1]) and (y[n-1] < y[n-2]): return True
+	return False
 
 def main():
-	print(euler(0,2,4,0.5))
+	euler(0.1, 0, 0) # intervalo h, posicion inicical, velocidad inicial
 
 main()
