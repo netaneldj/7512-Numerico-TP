a7db08c87b7c8e9a7cd56dd4e81832374ec16868
netaneldj <netaneldj97@gmail.com>
2018-06-20 17:04:23 -0300

    Agregue la aceleracion en el aire

--- metodoRungeKutta.py
+++ metodoRungeKutta.py
@@ -3,8 +3,10 @@ import math
 G = 9.807
 M = 87.464
 L0 = 51.549
-K1 = 49.366
-K2 = 1
+K1 = 9.8#49.366
+K2 = 1.25#1
+C1 = 4.873
+C2 = 1.5
 
 def ecuacion_iterativa_rungekutta(h,y0,f,v0):
 	q1 = h*f(y0)
@@ -31,11 +33,11 @@ def rungekutta4(h, y0, v0):
 	tiempo = [0]
 
 	while not cuarta_oscilacion:
-		#for i in range(500):
-		posicion.append(round(calcular_posicion(posicion, velocidad, h, n), 6))
-		velocidad.append(round(calcular_velocidad(posicion, velocidad, h, n), 5))
+		posicion.append(round(calcular_posicion(posicion, velocidad, h, n), 6)) #Redondea seis decimales
+		velocidad.append(round(calcular_velocidad(posicion, velocidad, h, n), 5)) #Redondea a 5 decimales
 		n+=1
-		aceleracion.append(round(calcular_aceleracion(posicion[n]), 5))
+		#aceleracion.append(round(calcular_aceleracion(posicion[n]), 5)) #Redondea aceleracion a 5 digitos
+		aceleracion.append(round(calcular_aceleracion_aire(posicion, velocidad, h, n), 5))
 		tiempo.append(round(n * h, 3))
 
 
@@ -57,6 +59,11 @@ def calcular_velocidad(y, v, h, n):
 def calcular_aceleracion(y):
 	if (y < L0): return G
 	return G -(K1/M)*math.pow(y-L0,K2)
+
+def calcular_aceleracion_aire(y,v,h,n):
+	if (y[n]<L0):
+		return G-(C1/M)*math.pow(abs(calcular_velocidad(y,v,h,n)),C2)#Calculo con el modulo de la velocidad 
+	return	G-(K1/M)*math.pow(y[n]-L0,K2)-(C1/M)*math.pow(abs(calcular_velocidad(y,v,h,n)),C2) #Calculo con el modulo de la velocidad
 
 def hay_un_maximo(y, n):
 	if (n > 1) and (y[n] < y[n-1]) and (y[n-1] > y[n-2]): return True
