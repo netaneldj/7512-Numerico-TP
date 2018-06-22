37a1922c23af7103fcac33415852c6dd3fa5ccca
netaneldj <netaneldj97@gmail.com>
2018-06-22 00:17:16 -0300

    Actualizacion graficador a 3 funciones

--- graficador.py
+++ graficador.py
@@ -7,12 +7,16 @@ import metodoRungeKuttaAire
 KMH = 3.6
 G = 9.807
 
-def graficar(n, titulo, dominio, imagen, x1, y1, x2, y2):
-
+def graficar(n, titulo, dominio, imagen, x1, y1, x2, y2, x3, y3):
+	
+	if(n==3):
+		plt.plot(x2, y2, linewidth=1, color='b')#, marker='*') Runge Kutta 4!
+		plt.plot(x3,y3,linewidth=1,color='r') #Runge-Kutta 4 Restriccion aceleracion!
+	
 	if (n==2):
-		plt.plot(x1, y1, linewidth=1, color='c')#, marker='*')
+		plt.plot(x2, y2, linewidth=1, color='b')#, marker='*') 
 
-	plt.plot(x2, y2, linewidth=1, color='b')#, marker='o')
+	plt.plot(x1, y1, linewidth=1, color='c')#, marker='o') Euler!
 
 	plt.xlabel(dominio)
 	plt.ylabel(imagen)
@@ -32,16 +36,16 @@ def cambio_unidades_aceleracion(aceleracion):
 	return a
 
 def grafico_comparativo():
-	#posEuler, velEuler, acelEuler, tiempoEuler = metodoEuler.euler(0.002, 0, 0)
+	posEuler, velEuler, acelEuler, tiempoEuler = metodoEuler.euler(0.002, 0, 0)
 	posRK4, velRK4, acelRK4, tiempoRK4 = metodoRungeKutta.rungekutta4(0.002, 0, 0)
 	posRKA4, velRKA4, acelRKA4, tiempoRKA4 = metodoRungeKuttaAire.rungekutta4(0.002, 0, 0)
 	
-	graficar(2, "grafico _posicion", "tiempo (s)", "posicion (m)", tiempoRKA4, posRKA4, tiempoRK4, posRK4)
+	graficar(3, "grafico _posicion", "tiempo (s)", "posicion (m)", tiempoRKA4, posRKA4, tiempoRK4, posRK4, tiempoEuler, posEuler)
 
-	graficar(2, "grafico _velocidad", "tiempo (s)", "velocidad (km/h)", tiempoRKA4, cambio_unidades_velocidad(velRKA4),
-			 tiempoRK4, cambio_unidades_velocidad(velRK4))
-	graficar(2, "grafico _aceleracion", "tiempo (s)", "aceleracion (g)", tiempoRKA4, cambio_unidades_aceleracion(acelRKA4),
-			 tiempoRK4, cambio_unidades_aceleracion(acelRK4))
+	graficar(3, "grafico _velocidad", "tiempo (s)", "velocidad (km/h)", tiempoRKA4, cambio_unidades_velocidad(velRKA4),
+			 tiempoRK4, cambio_unidades_velocidad(velRK4), tiempoEuler, cambio_unidades_velocidad(velEuler))
+	graficar(3, "grafico _aceleracion", "tiempo (s)", "aceleracion (g)", tiempoRKA4, cambio_unidades_aceleracion(acelRKA4),
+			 tiempoRK4, cambio_unidades_aceleracion(acelRK4), tiempoEuler, cambio_unidades_aceleracion(acelEuler))
 
 	
 	'''graficar(2, "grafico _posicion", "tiempo (s)", "posicion (m)", tiempoEuler, posEuler, tiempoRK4, posRK4)
